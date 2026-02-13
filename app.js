ff0327d6f4ef79084e94e27fc7c06a53ff6763fa
Avinash <avinash.developer.26@gmail.com>
2026-02-13 09:57:05 +0530

    added fade animations

--- app.js
+++ app.js
@@ -19,3 +19,15 @@ document.addEventListener("click", (e) => {
   }
 });
 
+const reveals = document.querySelectorAll(".reveal");
+
+const revealObserver = new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+    if(entry.isIntersecting){
+      entry.target.classList.add("active");
+    }
+  });
+}, {threshold : 0.2});
+
+reveals.forEach(el => revealObserver.observe(el));
+
